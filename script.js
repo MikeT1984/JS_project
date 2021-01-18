4278132f9d41aaf164f87d568ee65f1e664c2305
Mike <wwwtitov@yandex.ru>
2021-01-18 19:54:01 +0300

    add project

---
+++ script.js
@@ -0,0 +1,33 @@
+"use strict";
+let money = +prompt('Ваш бюджет на месяц?');
+let time = +prompt('Введите дату в формате YYYY-MM-DD');
+
+
+let appData = {
+    budjet: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: false
+};
+for (let i = 0; i < 2; i++) {
+    let a = prompt('Введите обязательную статью расходов в этом месяце');
+    let b = +prompt('Во сколько обойдется?');
+    if((typeof(a))=== 'string' && (typeof(a)) != null  && (typeof(b)) != null &&  a !='' && b != '' && a.length < 50) {
+        console.log("done");
+        appData.expenses[a] = b;
+    } else{
+
+    }
+};
+appData.moneyPerDay = appData.budjet / 30;
+
+alert(`'Ежедневный бюджет: ' ${appData.moneyPerDay}`);
+if(appData.moneyPerDay < 100) {
+console.log("Минимальный уровень достатка");
+}else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000){
+    console.log("Средний уровень достатка");
+}else if (appData.moneyPerDay > 2000) {
+    console.log("Высокий уровень достатка");
+}
